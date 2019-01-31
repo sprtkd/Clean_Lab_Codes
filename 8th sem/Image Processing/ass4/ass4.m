86e91a6c8821bbcdca2e89533d54e3a0f7ea474a
Suprotik Dey <supdey1@gmail.com>
2019-02-01 01:05:21 +0530

    Add files via upload

---
+++ 8th sem/Image Processing/ass4/ass4.m
@@ -0,0 +1,8 @@
+﻿clc;
+img=load('l256.dat');
+img=uint8(img);
+figure('Name','Actual'),imshow(img);
+%waitforbuttonpress;
+
+new_img = gray_slicer(img, 50, 170);
+figure('Name','sliced'),imshow(new_img);
---
+++ 8th sem/Image Processing/ass4/gray_slicer.m
@@ -0,0 +1,17 @@
+﻿function [ new_img ] = gray_slicer( img, c, d )
+[row,col,channels]=size(img);
+
+new_img = zeros(row,col,1);
+
+for i=1:row
+    for j=1:col
+        if img(i,j) >= c && img(i,j) <= d
+            new_img(i,j)=255;
+        else
+            new_img(i,j)=0;
+        end
+    end
+end
+
+new_img = uint8(new_img);
+endfunction
